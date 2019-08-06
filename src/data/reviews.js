8d0637bcba685f106a4fa0a5e1d611d2fcdeb788
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2019-08-06 09:51:47 -0700

    Working prototype of reviews banner

---
+++ src/data/reviews.js
@@ -0,0 +1,94 @@
+const reviews = [
+  {
+    reviewer: "Ramón C.",
+    title: "Great experience",
+    text: "Never heard something like that. Amazing. Super customer service.",
+  },
+  {
+    reviewer: "Christy K.",
+    title: "Very impressed",
+    text: "I was especially happy with the recent update via the app. The noise cancelling is awesome and the sound is amazing.",
+  },
+  {
+    reviewer: "Mats J.",
+    title: "Wonderful",
+    text: "I gave the headphones to my wife and she said she had never heard anything like this. Like being \"inside\" the music.",
+  },
+  {
+    reviewer: "Chazz B.",
+    title: "Blown Away",
+    text: "Best experience I have ever had from a pair of headphones, so much detail and new sounds that you would normally miss.",
+  },
+  {
+    reviewer: "J.A.D.",
+    title: "It's awesome",
+    text: "Everything they say about it is true. Believe it. Buy it. You won't regret it.",
+  },
+  {
+    reviewer: "Kendel D.",
+    title: "Do It",
+    text: "Best listening experience outside of being in the room with the band.",
+  },
+  {
+    reviewer: "Egil R.",
+    title: "Awesome product",
+    text: "I have stopped using my other professional headphones and just entirely switched to my nuraphones.",
+  },
+  {
+    reviewer: "Derek N.",
+    title: "Revolutionary device",
+    text: "The G2 update is proof that nura is not only a maker of fine headphones, but they're a great company too. They've earned my respect!",
+  },
+  {
+    reviewer: "Robin M.",
+    title: "Wow!",
+    text: "I had read good things about the nuraphone, but nothing prepared me for how good they are in real life, wow!",
+  },
+  {
+    reviewer: "Chuck D.",
+    title: "Mind blowing product",
+    text: "The nuraphone is unlike anything that you have ever experienced. Once you experience this you will never look at headphones the same again.",
+  },
+  {
+    reviewer: "Patricia P.",
+    title: "Fantastic",
+    text: "As I'm getting older my hearing has changed. Thats why I love nuraphone its magic, my music has never sounded so good. I love them.",
+  },
+  {
+    reviewer: "Jeremy L.",
+    title: "Love love love them",
+    text: "The battery life is amazing. Bass response and sound quality is like standing next to a speaker stack at a concert.",
+  },
+  {
+    reviewer: "Andrew G.",
+    title: "Amazing",
+    text: "Loving the deep sound of immersion.",
+  },
+  {
+    reviewer: "Stuart A.",
+    title: "The best",
+    text: "The new features of ANC and Social Mode have completed the functionality of these marvellous headphones.",
+  },
+  {
+    reviewer: "Anthony R.",
+    title: "Breakthrough in headphone technology",
+    text: "These cans represent a technological breakthrough and will be my \"go to\" headphones for a long time. Absolutely spectacular headphones.",
+  },
+  {
+    reviewer: "Joe D.",
+    title: "Everything I expected and more",
+    text: "The G2 update is bonkers - active noise cancelation! Thanks nura!",
+  },
+  {
+    reviewer: "Kial K.",
+    title: "Reinvigoratred my love for music",
+    text: "Nuraphones have changed the way I listen to music! I am in love with them!",
+  },
+  {
+    reviewer: "Andrea C.",
+    title: "Nothing but love",
+    text: "This is probably the last pair of headphones I will buy.",
+  },
+];
+
+export default reviews;
--- stories/index.js
+++ stories/index.js
@@ -1,14 +1,212 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { ThemeProvider } from 'atomize';
 import {
   Button,
+  Col,
   Div,
+  Icon,
+  Row,
+  Text,
 } from 'atomize';
+
+import theme from '../src/theme';
+import reviews from '../src/data/reviews';
+
+class ReviewContainerList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.containerListRef = React.createRef();
+    this.state = {
+      containerWidth: null,
+      containerHeight: null,
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    this.setState({
+      carouselWidth: this.containerListRef.current.parentNode.clientWidth,
+      containerHeight: this.containerListRef.current.clientHeight,
+    })
+    console.log(this.containerListRef.current.clientHeight);
+    console.log(this.containerListRef.current.getBoundingClientRect());
+
+  }
+
+  render () {    
+    const { itemIndex } = this.props;
+    const { carouselWidth, containerHeight } = this.state;
+
+    const slicedReviews = reviews.slice(0, reviews.length);
+
+    return (
+      <Div
+        ref={this.containerListRef}
+        d="flex"
+        bg="white"
+        id="review-containers"
+        style={{ whiteSpace: 'nowrap' }}
+        transform={{
+          xs: `translateX(${-itemIndex * carouselWidth / 2}px)`,
+          lg: `translateX(${-itemIndex * carouselWidth / 4}px)`,
+        }}
+        w={carouselWidth * slicedReviews.length + 'px'}
+      >
+        {
+          slicedReviews.map((review, index) => (
+            <Div
+              key={index}
+              d="inline-flex"
+              w={{
+                xs: carouselWidth / 2 + 'px',
+                lg: carouselWidth / 4 + 'px',
+              }}
+              className="review-container"
+              style={{
+                whiteSpace: 'pre-line',
+              }}
+            >
+              <Div 
+                d="flex"
+                flexDir="column"
+                justify="space-between"
+                className="review-wrapper"
+                p="1.5rem"
+              >
+                <Div
+                  d="flex" flexDir="column"
+                  w="100%" align="center"
+                  p={{ b: '1.5rem' }}
+                  className="review-text-content"
+                >
+                  <Div
+                    d="flex"
+                    flexDir="row"
+                    justify="center"
+                  >
+                    <Icon name="StarSolid" color="warning500" />
+                    <Icon name="StarSolid" color="warning500" />
+                    <Icon name="StarSolid" color="warning500" />
+                    <Icon name="StarSolid" color="warning500" />
+                    <Icon name="StarSolid" color="warning500" />
+                  </Div>
+                  <Text
+                    tag="h3" textSize="title"
+                    p={{ t: '1rem' , b: '0.5rem' }}
+                  >
+                    {review.title}
+                  </Text>
+                  <Div className="review-text-container">
+                    <Text
+                      tag="p" textSize="subheader"
+                      textAlign="center"
+                      style={{ lineHeight: '1.5rem' }}
+                      w="100%"
+                    >
+                      "{review.text}"
+                    </Text>
+                  </Div>
+                </Div>
+                <Div
+                  d="flex"
+                  flexDir="row"
+                  align="center"
+                  justify="center"
+                  bg="white"
+                  className="review-reviewer-name"
+                  textAlign="center"
+                  border={{ t: '1px solid '}}
+                  borderColor="gray400"
+                  p={{ t: '0.5rem' }}
+                >
+                  <Text tag="p" textSize="subheader">
+                    {review.reviewer}
+                  </Text>
+                  <Text tag="p" textSize="body" p={{ l: '0.5rem' }}>
+                    Verified Buyer
+                  </Text>
+                </Div>
+              </Div>
+            </Div>
+          ))
+        }
+      </Div>
+    );
+  }
+}
+
+class Carousel extends React.Component {
+  state = {
+    itemIndex: 0,
+  }
+
+  decrementIndex = () => {
+    this.setState(prevState => ({
+      itemIndex: prevState.itemIndex - 1,
+    }));
+  }
+
+  incrementIndex = () => {
+    this.setState(prevState => ({
+      itemIndex: prevState.itemIndex + 1,
+    }));
+  }
+
+  render () {
+    return (
+      <ThemeProvider theme={theme}>
+        <Div
+          w="100%" bg="white"
+          className="text-carousel-container"
+          p={{ y: "4rem" }}
+          fontFamily="primary"
+        >
+          <Row
+            className="text-carousel-wrapper"
+            w="100%"
+            m="none"
+            align="center"
+          >
+            <Col size={1} bg="white">
+              <Div w="100%" onClick={this.decrementIndex}>
+                <Button h="4rem" p="0px" w="100%" bg="transparent">
+                  <Icon name="LeftArrow" size="48px" color="gray800" />
+                </Button>
+              </Div>
+            </Col>
+            <Col
+              size={10}
+              bg="white"
+              className="text-carousel-content"
+              overflow="hidden"
+            >
+              <ReviewContainerList itemIndex={this.state.itemIndex} />
+            </Col>
+            <Col size={1} bg="white">
+              <Div w="100%" onClick={this.incrementIndex}>
+                <Button h="4rem" p="0px" w="100%" bg="transparent">
+                  <Icon name="RightArrow" size="48px" color="gray800" />
+                </Button>
+              </Div>
+            </Col>
+          </Row>
+        </Div>
+      </ThemeProvider>
+    );
+  }
+}
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button h="4rem" p={{ x : '2rem' }}>
-      Hello Button
-    </Button>
+    <Carousel />
   ));
