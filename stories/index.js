88c20a8ae6e5522c78084199de56cc2f955c5da9
Eddo W. Hintoso <eddo.hintoso+github@gmail.com>
2019-08-06 10:34:33 -0700

    Create generic reusable carousel component

--- stories/index.js
+++ stories/index.js
@@ -69,7 +69,7 @@ class ReviewContainerList extends React.Component {
                 xs: carouselWidth / 2 + 'px',
                 lg: carouselWidth / 4 + 'px',
               }}
-              className="review-container"
+              className="container"
               style={{
                 whiteSpace: 'pre-line',
               }}
@@ -143,7 +143,152 @@ class ReviewContainerList extends React.Component {
   }
 }
 
+const ReviewItem = ({ review }) => (
+  <Div 
+    d="flex"
+    flexDir="column"
+    justify="space-between"
+    className="review-wrapper"
+    p="1.5rem"
+  >
+    <Div
+      d="flex" flexDir="column"
+      w="100%" align="center"
+      p={{ b: '1.5rem' }}
+      className="review-text-content"
+    >
+      <Div
+        d="flex"
+        flexDir="row"
+        justify="center"
+      >
+        <Icon name="StarSolid" color="warning500" />
+        <Icon name="StarSolid" color="warning500" />
+        <Icon name="StarSolid" color="warning500" />
+        <Icon name="StarSolid" color="warning500" />
+        <Icon name="StarSolid" color="warning500" />
+      </Div>
+      <Text
+        tag="h3" textSize="title"
+        p={{ t: '1rem' , b: '0.5rem' }}
+      >
+        {review.title}
+      </Text>
+      <Div className="review-text-container">
+        <Text
+          tag="p" textSize="subheader"
+          textAlign="center"
+          style={{ lineHeight: '1.5rem' }}
+          w="100%"
+        >
+          "{review.text}"
+        </Text>
+      </Div>
+    </Div>
+    <Div
+      d="flex"
+      flexDir="row"
+      align="center"
+      justify="center"
+      bg="white"
+      className="review-reviewer-name"
+      textAlign="center"
+      border={{ t: '1px solid '}}
+      borderColor="gray400"
+      p={{ t: '0.5rem' }}
+    >
+      <Text tag="p" textSize="subheader">
+        {review.reviewer}
+      </Text>
+      <Text tag="p" textSize="body" p={{ l: '0.5rem' }}>
+        Verified Buyer
+      </Text>
+    </Div>
+  </Div>
+);
+
+class CarouselItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.containerListRef = React.createRef();
+    this.state = {
+      containerWidth: null,
+      containerHeight: null,
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = () => {
+    this.setState({
+      carouselWidth: this.containerListRef.current.parentNode.clientWidth,
+      containerHeight: this.containerListRef.current.clientHeight,
+    });
+  }
+
+  render () {    
+    const { itemIndex } = this.props;
+    const { carouselWidth, containerHeight } = this.state;
+
+    const carouselItems = this.props.children;
+
+    return (
+      <Div
+        ref={this.containerListRef}
+        d="flex"
+        bg="white"
+        class="carousel-item-containers"
+        style={{ whiteSpace: 'nowrap' }}
+        transform={{
+          xs: `translateX(${-itemIndex * carouselWidth / 2}px)`,
+          lg: `translateX(${-itemIndex * carouselWidth / 4}px)`,
+        }}
+        w={carouselWidth * carouselItems.length + 'px'}
+        transition="all 0.5s ease-in-out"
+      >
+        {carouselItems.map((item, index) => (
+          <Div
+            d="inline-flex"
+            w={{
+              xs: carouselWidth / 2 + 'px',
+              lg: carouselWidth / 4 + 'px',
+            }}
+            className="carousel-item-container"
+            style={{
+              whiteSpace: 'pre-line',
+            }}
+          >
+            {item}
+          </Div>
+        ))}
+      </Div>
+    );
+  }
+}
+
+
 class ReviewsCarousel extends React.Component {
+  render () {
+    return (
+      <Carousel>
+        {
+          reviews.map((review, index) => (
+            <ReviewItem key={index} review={review} />
+          ))
+        }
+      </Carousel>
+    );
+  }
+}
+
+class Carousel extends React.Component {
   state = {
     itemIndex: 0,
   }
@@ -161,23 +306,35 @@ class ReviewsCarousel extends React.Component {
   }
 
   render () {
+    const { itemIndex } = this.state;
+
     return (
       <ThemeProvider theme={theme}>
         <Div
           w="100%" bg="white"
-          className="text-carousel-container"
+          className="carousel-container"
           p={{ y: "4rem" }}
           fontFamily="primary"
         >
           <Row
-            className="text-carousel-wrapper"
+            className="carousel-wrapper"
             w="100%"
             m="none"
             align="center"
           >
-            <Col size={1} bg="white">
-              <Div w="100%" onClick={this.decrementIndex}>
-                <Button h="4rem" p="0px" w="100%" bg="transparent">
+            <Col
+              size={1} bg="white"
+              className="carousel-slide-left-container"
+            >
+              <Div
+                w="100%"
+                className="carousel-slide-left-wrapper"
+                onClick={this.decrementIndex}
+              >
+                <Button
+                  h="4rem" p="0px" w="100%" bg="transparent"
+                  className="carousel-slide-left-button"
+                >
                   <Icon name="LeftArrow" size="48px" color="gray800" />
                 </Button>
               </Div>
@@ -185,15 +342,28 @@ class ReviewsCarousel extends React.Component {
             <Col
               size={10}
               bg="white"
-              className="text-carousel-content"
+              className="carousel-content"
               overflow="hidden"
             >
-              <ReviewContainerList itemIndex={this.state.itemIndex} reviews={this.props.reviews} />
+              <CarouselItemList itemIndex={itemIndex} >
+                {this.props.children}
+              </CarouselItemList>
+              {/* <ReviewContainerList reviews={reviews} /> */}
             </Col>
-            <Col size={1} bg="white">
-              <Div w="100%" onClick={this.incrementIndex}>
-                <Button h="4rem" p="0px" w="100%" bg="transparent">
-                  <Icon name="RightArrow" size="48px" color="gray800" />
+            <Col
+              size={1} bg="white"
+              className="carousel-slide-right-container"
+            >
+              <Div
+                w="100%"
+                className="carousel-slide-right-wrapper"
+                onClick={this.decrementIndex}
+              >
+                <Button
+                  h="4rem" p="0px" w="100%" bg="transparent"
+                  className="carousel-slide-right-button"
+                >
+                  <Icon name="LeftArrow" size="48px" color="gray800" />
                 </Button>
               </Div>
             </Col>
